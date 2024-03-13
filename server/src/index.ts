e81a7c6a5150b6b5a727d67bbb96b32585d996d9
Tyler O'Neil <tyleroneildev@gmail.com>
2024-03-13 00:34:16 -0400

    Create gitignore

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
