3f77de6cb5d71be4f68e49c7476987df72d67ceb
Tyler O'Neil <tyleroneildev@gmail.com>
2024-03-13 00:35:52 -0400

    Add Typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app: express.Application = express();
+const port: number = Number(process.env.PORT) || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
