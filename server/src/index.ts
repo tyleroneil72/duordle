b0d8d37b8df12e92dd6fd3510e5b0549d3e524f2
Tyler O'Neil <tyleroneildev@gmail.com>
2024-03-13 01:03:14 -0400

    Add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+import cors from "cors";
 
 const app: express.Application = express();
+app.use(cors());
 const port: number = Number(process.env.PORT) || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
