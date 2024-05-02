bfd4106e94442c9cfe95b53baa6880299cf57c96
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-02 17:33:30 -0400

    Edit Header and Footer Styling

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,4 +1,8 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className='fixed bottom-0 left-0 w-full flex justify-center'>
+      Footer
+    </div>
+  );
 }
 export default Footer;
