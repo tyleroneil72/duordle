1e791ea618ca5b79220c86624e6695ef83581fca
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-02 17:26:12 -0400

    Add Header and Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RoomPage from "./pages/RoomPage";
 import ErrorPage from "./pages/ErrorPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='room/:roomCode' element={<RoomPage />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
