12b5fd08069ca714c65332a7d8c05744668efc92
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-02 17:35:15 -0400

    Add Lazy Loading components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import RoomPage from "./pages/RoomPage";
-import ErrorPage from "./pages/ErrorPage";
+import { Suspense, lazy } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const RoomPage = lazy(() => import("./pages/RoomPage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='room/:roomCode' element={<RoomPage />} />
-        <Route path='*' element={<ErrorPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='room/:roomCode' element={<RoomPage />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
