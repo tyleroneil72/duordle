b40d847f73af97401d42a0f6550a9e63c29f4819
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-02 18:18:59 -0400

    Add go home button

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
     <div className='text-center p-5'>
       <h1 className='text-3xl text-red-500'>Error</h1>
       <p>Sorry, there was a problem loading the page.</p>
+      <button
+        onClick={() => navigate("/")}
+        className='mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+      >
+        Go Home
+      </button>
     </div>
   );
 };
