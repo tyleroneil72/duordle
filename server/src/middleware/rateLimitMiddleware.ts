d23e181cdf55d7d8a9320a892c35159e61dba7c6
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-03 15:39:39 -0400

    Edit Requests to be 100

--- server/src/middleware/rateLimitMiddleware.ts
+++ server/src/middleware/rateLimitMiddleware.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 100, // limit each IP to 10 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 
