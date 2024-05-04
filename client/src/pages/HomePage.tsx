c935120ba637decb664a4b426e70a9bbcb62d428
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-04 16:03:08 -0400

    Create seperate fetch random word file

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,23 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { socket } from "../services/socket";
 import RoomButtons from "../components/RoomButtons";
-
-const URL = "http://localhost:3000";
-
-const fetchRandomWord = async () => {
-  try {
-    const response = await fetch(`${URL}/word/random`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch the word");
-    }
-    const data = await response.json();
-    return data.word.word;
-  } catch (error) {
-    console.error("Error fetching random word:", error);
-    alert("Error fetching random word: " + error);
-    return null; // Return null in case of error
-  }
-};
+import fetchRandomWord from "../services/fetchRandomWord";
 
 const generateRoomCode = () => {
   let result = "";
