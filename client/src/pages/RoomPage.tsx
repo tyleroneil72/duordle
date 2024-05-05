873ec7fa867260e5cee26accd4b8a4f26d383258
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-05 17:13:49 -0400

    Change members to status

--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -59,7 +59,7 @@ function RoomPage() {
       <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
         <h2 className='text-lg font-bold mb-4'>Room: {roomCode}</h2>
         <p className='mb-4'>Word: {word}</p>
-        <p>Members: {connectionStatus}</p>
+        <p>Status: {connectionStatus}</p>
       </div>
       <button
         className='bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
