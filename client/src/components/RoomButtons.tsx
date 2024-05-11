92d1e9960dc09f30acde14566a9a15bcacdec521
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-11 14:10:04 -0400

    Add Margin to Buttons

--- client/src/components/RoomButtons.tsx
+++ client/src/components/RoomButtons.tsx
@@ -9,13 +9,13 @@ function RoomButtons({
     <div className='flex justify-center my-4'>
       <button
         onClick={onCreate}
-        className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-1'
       >
         Create Room
       </button>
       <button
         onClick={onJoin}
-        className='bg-indigo-400 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        className='bg-indigo-400 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-1'
       >
         Join Room
       </button>
