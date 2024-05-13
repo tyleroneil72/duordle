489b399f281896d2df5e7e2939809bd3ab0fbf37
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-12 22:56:33 -0400

    Edit Loading Styling

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,4 +1,9 @@
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className='fixed inset-0 bg-white bg-opacity-50 flex justify-center items-center'>
+      <div className='animate-spin rounded-full h-12 w-12 border-4 border-t-transparent border-indigo-600'></div>
+    </div>
+  );
 };
+
 export default Loading;
