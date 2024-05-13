806c5c2638475bd1abbcd7503c025c079e8cd662
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-13 17:44:16 -0400

    Remove Unused Socket Export

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -2,7 +2,3 @@ import io from "socket.io-client";
 
 const URL: string = "/";
 export const socket = io(URL);
-
-export default socket;
-
-// TODO: Currently Exporting two socket instances, need to fix this
