e5ac2b903382b946869000b5e0e14f4521bb5f00
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-12 22:04:32 -0400

    Add Logs

--- start.sh
+++ start.sh
@@ -7,12 +7,15 @@ set -e
 set -x
 
 # Change directory to client and build
+echo "Building client..."
 cd client
 npm run build
 
 # Go back to the root directory and then into the server directory
+echo "Building server..."
 cd ../server
 npm run build
 
 # Start the server
+echo "Starting server in production mode..."
 npm run start
