1ef532c0ad21bcec750801d1634d1840c6fb8c7e
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-13 20:34:46 -0400

    Edit styling

--- client/src/components/RoomButtons.tsx
+++ client/src/components/RoomButtons.tsx
@@ -5,17 +5,17 @@ interface RoomButtonsProps {
 
 const RoomButtons: React.FC<RoomButtonsProps> = ({ onCreate, onJoin }) => {
   return (
-    <div className='flex justify-center my-4'>
+    <div className='flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 justify-center my-4'>
       <button
         onClick={onCreate}
-        className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-1'
+        className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline'
         title='Create a new room'
       >
         Create Room
       </button>
       <button
         onClick={onJoin}
-        className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-1'
+        className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline'
         title='Join an existing room'
       >
         Join Room
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -6,9 +6,7 @@ import Modal from "../components/Modal";
 import fetchRandomWord from "../utils/fetchRandomWord";
 import generateUniqueRoomCode from "../utils/generateUniqueRoomCode";
 
-interface HomePageProps {}
-
-const HomePage: React.FC<HomePageProps> = () => {
+const HomePage: React.FC = () => {
   const navigate = useNavigate();
 
   const handleCreateRoom = async () => {
@@ -65,10 +63,12 @@ const HomePage: React.FC<HomePageProps> = () => {
   };
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-indigo-300'>
       <Modal />
-      <h1 className='text-4xl font-bold text-indigo-600 mb-6'>Duordle</h1>
-      <div className='w-full max-w-lg bg-white shadow-md rounded-lg overflow-hidden p-6'>
+      <h1 className='text-5xl font-extrabold text-indigo-50 mb-6 sm:mb-10'>
+        Duordle
+      </h1>
+      <div className='w-full max-w-md bg-indigo-200 shadow-lg rounded-lg p-6 mt-10 sm:mt-20'>
         <RoomButtons onCreate={handleCreateRoom} onJoin={handleJoinRoom} />
       </div>
       <button
