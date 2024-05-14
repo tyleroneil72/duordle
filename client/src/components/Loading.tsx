c448607d1a524c3d6d4a33e8941f2b96587484bc
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-13 20:43:02 -0400

    Change Loading Page Background Colour

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -2,7 +2,7 @@ interface LoadingProps {}
 
 const Loading: React.FC<LoadingProps> = () => {
   return (
-    <div className='fixed inset-0 bg-white bg-opacity-50 flex justify-center items-center'>
+    <div className='fixed inset-0 bg-indigo-300 bg-opacity-50 flex justify-center items-center'>
       <div className='animate-spin rounded-full h-12 w-12 border-4 border-t-transparent border-indigo-600'></div>
     </div>
   );
