d5e75e50a862f3fa9dc67b20bd61424401107b99
Tyler O'Neil <tyleroneildev@gmail.com>
2024-05-13 20:08:27 -0400

    Fix Copyable Board

--- client/src/components/GameOver.tsx
+++ client/src/components/GameOver.tsx
@@ -20,6 +20,7 @@ const GameOver: React.FC<GameOverProps> = ({
   useEffect(() => {
     const createGameState = () => {
       return board
+        .filter((row) => row.some((letter) => letter !== "")) // Filter out empty rows
         .map((row) =>
           row
             .map((letter, index) => {
@@ -49,29 +50,25 @@ const GameOver: React.FC<GameOverProps> = ({
   return (
     <div className='fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center'>
       <div className='bg-white p-4 rounded shadow-lg max-w-md w-full'>
-        <h2 className='text-lg font-bold mb-4'>Game Over</h2>
+        <h2 className='text-lg font-bold mb-4 text-center'>Game Over</h2>
         <div className='text-center text-lg font-bold mb-4'>
           {win ? "You won!" : "You lost!"}
         </div>
-        <div className='whitespace-pre mb-4'>{gameState}</div>
-        <button
-          className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2'
-          onClick={copyToClipboard}
-        >
-          Copy to Clipboard
-        </button>
-        <button
-          className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2'
-          onClick={onClose}
-        >
-          Close
-        </button>
-        <button
-          className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-          onClick={() => (window.location.href = "/")}
-        >
-          Leave Room
-        </button>
+        <div className='whitespace-pre mb-4 text-center'>{gameState}</div>
+        <div className='flex justify-center space-x-2'>
+          <button
+            className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+            onClick={copyToClipboard}
+          >
+            Copy to Clipboard
+          </button>
+          <button
+            className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
