275c7f8647ad04f9aff08cead27bbb144bf7f916
Tyler O'Neil <tyleroneildev@gmail.com>
2024-07-16 18:16:53 -0400

    Remove Header and Footer comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
 import Loading from "./components/Loading";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
