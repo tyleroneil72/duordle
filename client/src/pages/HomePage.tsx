9f040301c96c0f09fe67b0b3de3084e6463292ab
Tyler O'Neil <tyleroneildev@gmail.com>
2024-08-25 16:28:39 -0400

    Make room code always upper case

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -19,7 +19,7 @@ const HomePage: React.FC = () => {
   };
 
   const handleJoinRoom = () => {
-    const joinRoomCode = prompt('Enter room code:');
+    const joinRoomCode = prompt('Enter room code:')?.toUpperCase();
     if (!joinRoomCode) return;
 
     setupRoomEventListeners(joinRoomCode, 'join');
