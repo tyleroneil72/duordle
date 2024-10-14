207046e61fed56a8273319fb7b2075f1fb87d640
Tyler O'Neil <tyleroneildev@gmail.com>
2024-10-13 20:29:31 -0400

    Change api rate limit to 50 per minute

--- server/src/middleware/rateLimitMiddleware.ts
+++ server/src/middleware/rateLimitMiddleware.ts
@@ -3,7 +3,7 @@ import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 10, // limit each IP to 10 requests per windowMs
+  max: 50, // limit each IP to 50 requests per windowMs
   statusCode: StatusCodes.TOO_MANY_REQUESTS,
   message: JSON.stringify({
     message: ReasonPhrases.TOO_MANY_REQUESTS
