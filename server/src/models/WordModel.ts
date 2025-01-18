ba3ddeb6ec3aab0aedfc3d3bea263398f09aa6df
Tyler O'Neil <tyleroneildev@gmail.com>
2025-01-18 13:36:28 -0500

    Update mongoose types

--- server/src/models/WordModel.ts
+++ server/src/models/WordModel.ts
@@ -1,6 +1,7 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema, Types } from 'mongoose';
 
 interface IWord extends Document {
+  _id: Types.ObjectId;
   word: string;
   difficulty: string;
   length: number;
