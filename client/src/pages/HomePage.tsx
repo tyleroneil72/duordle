fd074c2a14e9b856d5ad9678335ea82ac38f8083
Tyler O'Neil <tyleroneildev@gmail.com>
2025-05-01 21:42:05 -0400

    feature: create landing card

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 import { FaCog } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import logo from '../assets/images/logo.png';
+import LandingCard from '../components/LandingCard';
 import PopupModal from '../components/PopupModal';
-import RoomButtons from '../components/RoomButtons';
 import RoomCodeModal from '../components/RoomCodeModal';
 import { socket } from '../services/socket';
 import fetchRandomWord from '../utils/fetchRandomWord';
@@ -72,35 +71,26 @@ const HomePage: React.FC = () => {
     }
   };
 
-  const handleRoomNotFoundModalClose = () => {
-    setIsRoomNotFoundModalOpen(false);
-  };
-
-  const handleRoomFullModalClose = () => {
-    setIsRoomFullModalOpen(false);
-  };
-
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center bg-indigo-300 py-8 sm:py-12 lg:py-24'>
+    <div className='flex min-h-screen flex-col items-center justify-center bg-indigo-300 px-4 py-8 sm:py-12 lg:py-24'>
       <PopupModal
         isOpen={isRoomNotFoundModalOpen}
-        onClose={handleRoomNotFoundModalClose}
+        onClose={() => setIsRoomNotFoundModalOpen(false)}
         title='Room Not Found'
-        message='The room you are trying to join does not exist. Please check the room code and try again.'
+        message='The room you are trying to join does not exist.'
       />
       <PopupModal
         isOpen={isRoomFullModalOpen}
-        onClose={handleRoomFullModalClose}
-        title='Room is Full'
-        message='The room you are trying to join is already full. Please try a different room.'
+        onClose={() => setIsRoomFullModalOpen(false)}
+        title='Room Full'
+        message='The room you are trying to join is already full.'
       />
-      <img src={logo} height='200px' width='300px' alt='Duordle Image' title='Duordle Image' loading='lazy' />
-      <div className='mt-8 w-11/12 max-w-sm rounded-lg bg-indigo-100 p-4 shadow-lg sm:mt-12 sm:max-w-md sm:p-6 lg:max-w-lg'>
-        <RoomButtons onCreate={handleCreateRoom} onJoin={handleJoinRoom} />
-      </div>
+
+      <LandingCard onCreate={handleCreateRoom} onJoin={handleJoinRoom} />
+
       <button
         onClick={() => navigate('/info')}
-        className='fixed right-4 bottom-4 flex cursor-pointer items-center justify-center rounded-full bg-indigo-600 p-3 text-white shadow-lg hover:bg-indigo-700'
+        className='fixed right-4 bottom-4 flex items-center justify-center rounded-full bg-indigo-600 p-3 text-white shadow-lg hover:bg-indigo-700'
         title='Info'
       >
         <FaCog size={24} />
